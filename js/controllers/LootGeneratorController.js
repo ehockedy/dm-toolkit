74efab3632e04dc5a284f87c928409dfd4854894
Edward Hockedy <edhockedy@hotmail.com>
2020-06-28 09:17:38 +0100

    Improved loot generator
    Added buttons for different amount of loot
    Added all possible rarity options for low and med

--- js/controllers/LootGeneratorController.js
+++ js/controllers/LootGeneratorController.js
@@ -4,11 +4,57 @@ app.controller('LootGeneratorController',
     $scope.screen_text = "";
     loot_service.success(function(data) {
       $scope.loot_json = data;
-    }),
-    $scope.generate_loot = function(level) {
-      $scope.loot_count = Object.keys($scope.loot_json[level]).length
-      var rand_idx = Math.ceil(Math.random()*$scope.loot_count)-1
-      $scope.loot = $scope.loot_json[level][rand_idx].name;
+    });
+
+    var rarity = {
+      COMMON: {
+        name: "common",
+        chance : 0.6
+      },
+      UNCOMMON: {
+        name: "uncommon",
+        chance: 0.35
+      },
+      RARE: {
+        name: "rare",
+        chance: 0.04
+      },
+      VERY_RARE: {
+        name: "very rare",
+        chance: 0.01
+      }
+    };
+
+    console.assert(
+      rarity.COMMON.chance + 
+      rarity.UNCOMMON.chance +
+      rarity.RARE.chance +
+      rarity.VERY_RARE.chance
+      == 1);
+
+    function generate_rarity() {
+      // Generates a random number, then based on the above
+      // json returns the string
+      var rand = Math.random();
+      console.log(rand)
+      var cumulative = 0;
+      for (var key in rarity) {
+        cumulative += rarity[key].chance;
+        if (rand < cumulative) {
+          return rarity[key].name;
+        }
+      }
+    };
+
+    $scope.generate_loot = function(level, num) {
+      $scope.loot = ""
+      for (var n = 0; n < num; n++) {
+        var r = generate_rarity();
+        var loot_count = Object.keys($scope.loot_json[level][r]).length;
+        var rand_idx = Math.ceil(Math.random()*loot_count)-1;
+        $scope.loot += $scope.loot_json[level][r][rand_idx].name;
+        if (n < num -1) $scope.loot += ", ";
+      }
     }
   }
   ]
