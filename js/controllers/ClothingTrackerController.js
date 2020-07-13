7c554e1f49a2da6cff3ff4732578352a769a254e
Edward Hockedy <edhockedy@hotmail.com>
2020-07-13 10:34:12 +0100

    Added name and description text boxes to clothing tracker

--- js/controllers/ClothingTrackerController.js
+++ js/controllers/ClothingTrackerController.js
@@ -1,11 +1,40 @@
 app.controller('ClothingTrackerController',
   ['$scope',
   function($scope) {
+    var items = {}
+    var activeItem
+    $scope.itemNameFill = ""
+    $scope.itemDescrFill = "" 
+
     $scope.click = function(param) {
-        console.log(`click on ${param}`);
+      console.log(`click on ${param}`);
+      if (!(param in items)) {
+        console.log("Adding", param)
+        items[param] = {
+          "value": "",
+          "description": ""
+        }
+      }
+      activeItem = param
+      $scope.itemNameFill = items[param]["value"]
+      $scope.itemDescrFill = items[param]["description"]
     }
+
     $scope.hover = function(param) {
         console.log(`hover over ${param}`);
+    }
+    $scope.key_press = function(keyCode) {
+      if (keyCode != 13) return;  // Enter key
+      
+      if (activeItem == undefined) {
+        alert("Please select a body area first");
+        return
+      }
+
+      // type can be "value" or "description"
+      items[activeItem]["value"] = $scope.itemNameFill
+      items[activeItem]["description"] = $scope.itemDescrFill
+      console.log("Updated", activeItem, "to have", $scope.itemNameFill)
     }
   }
   ]
